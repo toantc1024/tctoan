3bd962414864770b8869c36f828c8b8628af95c9
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-09-16 10:34:05 +0700

    Upload site

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import LaunchIcon from '@mui/icons-material/Launch';
-const Project = ({data}) => {
-	const {link, info, imageUrl, title} = data;
-	return (
-		<div className="project">
-			<a className="f6 dim no-underline ph3 pv2 mb2 dib" target="_blank" href={link}>
-				<span className="links">
-					<span>
-						{title}
-					</span>
-					<span>
-						<LaunchIcon />
-					</span>
-				</span>
-			</a>
-			<div>{info}</div>
-			<div className="image">
-				<img src={imageUrl} alt={title}/>
-			</div>
-		</div>
-	)
-}
+import React from "react";
+import LaunchIcon from "@mui/icons-material/Launch";
+const Project = ({ data }) => {
+  const { github, demo, info, title } = data;
+  return (
+    <div className="project">
+      <h1>{title}</h1>
+      <div style={{ display: "flex" }}>
+        <a
+          className="f6 dim no-underline ph3 pv2 mb2 dib"
+          target="_blank"
+          href={github}
+        >
+          <span className="links">
+            <span>Github repo</span>
+            <span>
+              <LaunchIcon />
+            </span>
+          </span>
+        </a>
+        <a
+          className="f6 dim no-underline ph3 pv2 mb2 dib"
+          target="_blank"
+          href={demo}
+        >
+          <span className="links">
+            <span>Deployed site</span>
+            <span>
+              <LaunchIcon />
+            </span>
+          </span>
+        </a>
+      </div>
+      <div>{info}</div>
+      <div className="image">{/* <img src={imageUrl} alt={title} /> */}</div>
+    </div>
+  );
+};
 
 export default Project;
