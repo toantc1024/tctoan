bec951b932a638b1258eea6843464f1997d55a22
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-09-24 15:02:33 +0700

    UPDATE: ADD CH6

--- src/components/Projects/project-info.js
+++ src/components/Projects/project-info.js
@@ -11,22 +11,18 @@ export const project_data = [
     github: "https://github.com/toantc1024/Ch05_EmailList",
     info: "Sử dụng JSTL để hiển thị danh sách email đã nhập. Sử dụng JSP để hiển thị trang web.",
   },
-  // {
-  //   title: 'Smart Brain',
-  //   link: 'https://github.com/toantc1024/smartbrain',
-  //   info:'[React App] [Fullstack] Recognize human face in image and add bounding box to these faces with format *.jpg',
-  //   imageUrl: 'https://user-images.githubusercontent.com/60417892/210522419-b5742e3c-72c9-43dd-9153-959c11bb707f.png',
-  // },
-  // {
-  //   title: 'Robofriends',
-  //   link: 'https://github.com/toantc1024/robofriends',
-  //   info:'[React App] [Front-end] helps filter Robo name cards using fetch API and map.',
-  //   imageUrl: 'https://user-images.githubusercontent.com/60417892/209813012-f811e2fc-a3d4-43da-98d0-440b179f49fe.png',
-  // },
-  // {
-  //   title: 'EVTD - Browser Dictionary',
-  //   link: 'https://github.com/toantc1024/EVTD-pre/blob/main/Help.md',
-  //   'info': 'Chrome Extension helps translate multi-language words in page with double-click (or select). Save words which translated and make word-book to add image, example, audio for these words.',
-  //   imageUrl: 'https://user-images.githubusercontent.com/60417892/181923690-09da0baa-56df-417c-a032-48167a91e05d.png'
-  // }
+  {
+    title: "Chapter 06 - Exercise1",
+    demo: "https://deploy-wdmm.onrender.com/ch06_ex1_email",
+    info: "Sử dụng JSTL để hiện năm, includes header và footer",
+    github:
+      "https://github.com/toantc1024/Servlet-Java-SEM-I-24-25/tree/main/ch06_ex1_email",
+  },
+  {
+    title: "Chapter 06 - Exercise2",
+    demo: "https://deploy-wdmm.onrender.com/ch06_ex2_email",
+    info: "Sửa bài Survey, tạo thêm điều kiện cho JSTL ở thanks.jsp hiện thi contact via nếu user nhận mail",
+    github:
+      "https://github.com/toantc1024/Servlet-Java-SEM-I-24-25/tree/main/ch06_ex2_survey",
+  },
 ];
